d3d2a5d5b76789150d436da671c29b6b01e2c169
udit1567 <uditmaurya2006@gmail.com>
2026-07-02 10:06:36 +0530

    new

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ dQIDAQAB
 -----END PUBLIC KEY-----"""
 
 # 4. Q3: 12-Factor Config (Manually merge the variables)
-Q3_PORT = 8549
+Q3_PORT = 8142
 Q3_WORKERS = 5
 Q3_DEBUG = False
 Q3_LOG_LEVEL = "info"
